0335549d3a82829ddb7a855b68fdc769e9a2ac56
NikashS <nikashsethi@gmail.com>
2016-06-24 13:21:02 -0400

    sync with updated master files

--- js/parallax.js
+++ js/parallax.js
@@ -5,7 +5,11 @@ function simpleParallax() {
 
     //Every element with the class "scroll" will have parallax background 
     //Change the "0.3" for adjusting scroll speed.
+<<<<<<< HEAD
+    $('.scroll').css('background-position', '0' + -(scrolled * 0.9) + 'px');
+=======
     $('.scroll').css('background-position', '0' + -(scrolled * 0.1) + 'px');
+>>>>>>> fc9a4a941de91ed88ddd53b5e76690fd320438cb
 }
 //Everytime we scroll, it will fire the function
 $(window).scroll(function (e) {
@@ -19,7 +23,11 @@ function simpleParallax2() {
 
     //Every element with the class "scroll" will have parallax background 
     //Change the "0.3" for adjusting scroll speed.
+<<<<<<< HEAD
+    $('.scroll2').css('background-position', '0' + -(scrolled * 0.8) + 'px');
+=======
     $('.scroll2').css('background-position', '0' + -(scrolled * 0.5) + 'px');
+>>>>>>> fc9a4a941de91ed88ddd53b5e76690fd320438cb
 }
 //Everytime we scroll, it will fire the function
 $(window).scroll(function (e) {
